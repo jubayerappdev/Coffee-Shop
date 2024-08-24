177cdbce8d59be11ce2ec5b38fed07e22aa60722
MD. Jubayer Ahmed <63640474+jubayerappdev@users.noreply.github.com>
2024-08-24 10:51:14 +0600

    initial commit

---
+++ app/src/main/java/com/example/coffeeshop/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.coffeeshop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeeshop.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this@IntroActivity,MainActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
+    }
+}
